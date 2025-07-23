1a2914ed6c92b4804911f76c0cae5aa1fd0ea5bf
Nguyễn Văn Kiên <nguyenvankien@Admins-Mac-mini.local>
2025-07-23 14:48:42 +0700

    fix: set resizeToAvoidBottomInset to false to avoid render overflow when keyboard is showed

--- lib/screens/add_new_task_screen.dart
+++ lib/screens/add_new_task_screen.dart
@@ -16,6 +16,7 @@ class AddNewTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
           color: Color(0xffF1F5F9),
